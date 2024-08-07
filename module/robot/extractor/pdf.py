75fbbd99323f9379f3eab16dcea5108fd74966a3
joao_almeida <jp080496@gmail.com>
2024-08-06 21:29:23 -0300

    f

--- module/robot/extractor/pdf.py
+++ module/robot/extractor/pdf.py
@@ -3,48 +3,38 @@ import os
 from io import BytesIO
 
 
+def process_pdf(file):
+    doc = {
+        'extractor': 'PyPDF2',
+        'pages': [],
+    }
+    text_ = ""
+    reader = PyPDF2.PdfReader(file)
+    for page_num in range(len(reader.pages)):
+        page = reader.pages[page_num]
+        text = page.extract_text().strip()
+        doc['pages'].append(text)
+        text_+=text
+
+    if not text_:
+        raise Exception('Não foi possível extrair texto do PDF')
+
+    return doc
+
 def extract_text_from_pdf_path(pdf_path):
     
     nome_arquivo, extensao = os.path.splitext(os.path.basename(pdf_path))
     
-    doc = {
-        'filename': nome_arquivo + extensao,
-        'extractor': 'PyPDF2',
-        'pages': [],
-    }
-    text_ = ""
-    
     with open(pdf_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        for page_num in range(len(reader.pages)):
-            page = reader.pages[page_num]
-            text = page.extract_text().strip()
-            doc['pages'].append(text)
-            text_+=text
-
-    if not text_:
-        raise Exception('Não foi possível extrair texto do PDF')
+        doc = process_pdf(file)
+    
+    doc['filename'] = nome_arquivo + extensao
     
     return doc
 
 def extract_text_from_pdf_bytes(pdf_bytes):
-    
-    doc = {
-        'extractor': 'PyPDF2',
-        'pages': [],
-    }
-    
     # Usando BytesIO para tratar o conteúdo do PDF como um arquivo
-    text_ = ""
     with BytesIO(pdf_bytes) as file_stream:
-        reader = PyPDF2.PdfReader(file_stream)
-        for page_num in range(len(reader.pages)):
-            page = reader.pages[page_num]
-            text = page.extract_text().strip()
-            doc['pages'].append(text)
-            text_+=text
-    
-    if not text_:
-        raise Exception('Não foi possível extrair texto do PDF')
-    
+        doc = process_pdf(file_stream)
+
     return doc
--- module/robot/extractor/tesseract.py
+++ module/robot/extractor/tesseract.py
@@ -8,32 +8,7 @@ tesseract_cmd = r'F:\Program Files\Tesseract-OCR\tesseract.exe'  # Atualize com 
 pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
 
 
-def extract_text_tesseract_pdf_path(pdf_path):
-    
-    nome_arquivo, extensao = os.path.splitext(os.path.basename(pdf_path))
-    images = convert_from_path(pdf_path, poppler_path=poppler_path)
-    
-    doc = {
-        'filename': nome_arquivo + extensao,
-        'pages': [],
-        'extractor': 'pytesseract',
-    }
-    
-    text_ = ""
-    for image in images:
-        text = pytesseract.image_to_string(image, lang='por').strip()
-        doc['pages'].append(text)
-        text_+=text
-    
-    if not text_:
-        raise Exception('Não foi possível extrair texto do PDF')
-    
-    return doc
-
-def extract_text_tesseract_pdf_bytes(pdf_bytes):
-    
-    images = convert_from_bytes(pdf_bytes, poppler_path=poppler_path)
-    
+def extract_text(images):
     doc = {
         'extractor': 'pytesseract',
         'pages': [],
@@ -48,5 +23,24 @@ def extract_text_tesseract_pdf_bytes(pdf_bytes):
     
     if not text_:
         raise Exception('Não foi possível extrair texto do PDF')
+    
+    return doc
+
+def extract_text_tesseract_pdf_path(pdf_path):
+    
+    nome_arquivo, extensao = os.path.splitext(os.path.basename(pdf_path))
+    images = convert_from_path(pdf_path, poppler_path=poppler_path)
+    
+    doc = extract_text(images)
+    doc['filename'] = nome_arquivo + extensao
+    
+    return doc
+
+def extract_text_tesseract_pdf_bytes(pdf_bytes):
+    
+    images = convert_from_bytes(pdf_bytes, poppler_path=poppler_path)
+    
+    # Converter PDF em imagens
+    doc = extract_text(images)
     
     return doc
