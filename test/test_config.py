cf4f48ba44df149b080dd667663c606927acdeec
joao_almeida <jp080496@gmail.com>
2024-08-06 21:17:01 -0300

    f

--- test/test_config.py
+++ test/test_config.py
@@ -1,8 +1,2 @@
 from module import settings
 
-
-def test_conexao_padrao():
-    assert type(settings.CONEXAO_ESPELHO) == dict
-    
-def test_conexao_email():
-    assert type(settings.CONEXAO_EMAIL) == dict
